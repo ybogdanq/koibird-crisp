bf19df0a33bcf999f62fc674866bdcd3aa46ced7
Bohdan Yereshchenko <Lord1mix7926@gmail.com>
2022-03-10 13:25:27 +0100

    Backup before setting up gulp

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -1,6 +1,8 @@
 const burgerMenu = document.querySelector(".header__burger-menu");
 const burgerMenuOpenBtn = document.querySelector(".header__burger_open");
 const burgerMenuCloseBtn = document.querySelector(".header__burger_close");
+const searchBtn = document.querySelector(".search__button");
+const searchWrapper = document.querySelector(".header__search");
 const body = document.body;
 
 burgerMenuOpenBtn.addEventListener("click", () => {
@@ -10,6 +12,9 @@ burgerMenuOpenBtn.addEventListener("click", () => {
 burgerMenuCloseBtn.addEventListener("click", () => {
   burgerMenu.classList.remove("active");
   body.classList.remove("lock-scroll");
+});
+searchBtn.addEventListener("click", () => {
+  searchWrapper.classList.toggle("active");
 });
 
 document.addEventListener("DOMContentLoaded", () => {
